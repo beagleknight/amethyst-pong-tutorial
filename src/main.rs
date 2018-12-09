ccbedbafd4fc3147bdeef9bb5af35c99cfee6797
beagleknight <david.morcillo@gmail.com>
2018-12-10 00:44:51 +0100

    Finished 4.3 tutorial

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+extern crate amethyst;
+
+mod pong;
+use crate::pong::Pong;
+
+mod systems;
+
+use amethyst::core::transform::TransformBundle;
+use amethyst::input::InputBundle;
+use amethyst::prelude::*;
+use amethyst::renderer::{DisplayConfig, DrawFlat2D, Pipeline, RenderBundle, Stage};
+
+fn main() -> amethyst::Result<()> {
+    use amethyst::utils::application_root_dir;
+
+    amethyst::start_logger(Default::default());
+
+    let path = format!("{}/resources/display_config.ron", application_root_dir());
+    let config = DisplayConfig::load(&path);
+
+    let binding_path = format!("{}/resources/bindings_config.ron", application_root_dir());
+
+    let pipe = Pipeline::build().with_stage(
+        Stage::with_backbuffer()
+            .clear_target([0.0, 0.0, 0.0, 1.0], 1.0)
+            .with_pass(DrawFlat2D::new()),
+    );
+
+    let game_data = GameDataBuilder::default()
+        .with_bundle(RenderBundle::new(pipe, Some(config)).with_sprite_sheet_processor())?
+        .with_bundle(TransformBundle::new())?
+        .with_bundle(InputBundle::<String, String>::new().with_bindings_from_file(binding_path)?)?
+        .with(systems::PaddleSystem, "paddle_system", &["input_system"]);
+
+    let mut game = Application::new("./", Pong, game_data)?;
+
+    game.run();
+
+    Ok(())
+}
